17718b34c4f10e59f9db2e9e7daf562d81b9ff93
SirEvgen <sandrofa6666666@gmail.com>
2024-06-12 17:23:23 +0300

    Update module_4_1.py

--- Module4_1/module_4_1.py
+++ Module4_1/module_4_1.py
@@ -1,9 +1,9 @@
 from fake_math import divide as fk
 from true_math import divide as tr
 
-print('Здесь на 0 нельзя делить')
+print('\033[96mЗдесь на 0 делить нельзя\033[0m')
 result1 = fk(float(input('Введите делимое: ')), float(input('Введите делитель: ')))
 print(result1)
-print('Здесь на 0 можно делить')
+print('\033[96mЗдесь на 0 делить можно\033[0m')
 result2 = tr(float(input('Введите делимое: ')), float(input('Введите делитель: ')))
 print(result2)
